c3adbd1ea58aaa8dd10a37a6f547c8ac599bd382
Gabriel Ribeiro <gcrs2018@gmail.com>
2025-09-16 21:37:51 -0300

    Mudanças

--- NewProject.java
+++ NewProject.java
@@ -2,5 +2,8 @@ public class NewProject {
     public static void main(String[] args) {
         System.out.println("Commit on github!");
         System.out.println("Production commit");
+        
+        System.out.println("Second commit");
+        System.out.println("New commit");
     }
 }
